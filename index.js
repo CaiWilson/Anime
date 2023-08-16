0812dd8fdddd7fc7a50c1b81516f28e7068f67fd
Wilson <cwx763360646@163.com>
2023-08-16 15:26:09 +0800

    first-commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// 导入模块
+let cheerio = require("cheerio")
+let axios = require("axios")
+let mysql = require("mysql")
+
+// 数据库配置
+const options = {
+    host:"localhost",
+    //port:3306,
+    user:"root",
+    password:"1234",
+    database:"anime"
+}
+// 连接数据库
+// const con = mysql.createConnection(options)
+// con.connect()
+
+// 获取动漫列表
+async function getAnimeList() {
+    let res = await axios.get("https://www.ppxdm.com/list/2.html")
+    // $(".module-class-items:first-child a").each((i,ele)=>{
+    //     let href = $(ele).attr("href")
+    //     console.log(href)
+    // })
+
+}
+getAnimeList()
